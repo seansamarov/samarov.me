4dd51baf7164d1a5fad9dc48cd13238c4c779a3b
Sean Samarov <sean@samarov.me>
2025-09-01 16:32:58 -0400

    move cta buttons into nav, add media

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
+import {build} from "vite";
 
+// TODO add html minifying and style enforcement
+// TODO add CSS style enforcement
+// TODO add impage optimization and make it somewhat automatic.
+// TODO probably gonna want to add srcsets for images, and preview images for videos
+// TODO Software is never completed, only abandoned.
 export default {
+    build: {
+        rollupOptions: {
+            input: {
+                home: "index.html",
+                music: "music.html"
+            }
+        }
+    },
     plugins: [
         handlebars({
             partialDirectory: resolve(__dirname, 'src/partials'),
